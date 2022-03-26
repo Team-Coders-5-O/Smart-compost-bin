61306cea81ae25de5b9aceebce66fba2606fd728
Vinuka Aksika <86453897+Vinuxbro@users.noreply.github.com>
2022-03-26 23:22:01 +0530

    about page

---
+++ src/components/About.js
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import Logo from "../IMAGES/logo.png";
+import "./Home.css";
+import "./Navbar.css";
+
+function Community() {
+  return (
+    // <div>
+    //   <h1>This is the home page</h1>
+    //   <Link to="about">Click to view our about page</Link>
+    //   <Link to="contact">Click to view our contact page</Link>
+    // </div>
+    <div>
+    <header>
+        <Link to="/"><img src={Logo} /></Link>
+        <nav>
+            <ul class="nav_links">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/community">Community</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/contact">Contact Us</Link></li>
+            </ul>
+        </nav>
+        <Link class="cta" to="/logintype"><button>Login</button></Link>
+    </header>
+    </div>
+  )
+}
+
+export default Community;
