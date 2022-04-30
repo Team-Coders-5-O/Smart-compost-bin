627dfa3855b069ecf8cf458e839f964d883a9514
Mathusan <mathusanadded@gmail.com>
2022-05-01 00:56:42 +0530

    Update temperature_sense.py

--- temperature_sense.py
+++ temperature_sense.py
@@ -5,6 +5,12 @@ DHT_SENSOR = Adafruit_DHT.DHT11
 DHT_PIN = 4
 
 def get_temp():
-   humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+   humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
    return humidity , temperature
+
+
+if __name__ == '__main__':
+    while True:
+        print(get_temp())
+        time.sleep(1)
 
