438ceb3660551dddb0ff1c358a63664abb33c462
Mathusan <mathusanadded@gmail.com>
2022-05-01 00:48:25 +0530

    Update moisture_sense.py

--- moisture_sense.py
+++ moisture_sense.py
@@ -17,13 +17,22 @@ mcp = MCP.MCP3008(spi, cs)
 # create an analog input channel on pin 0
 chan = AnalogIn(mcp, MCP.P0)
 
-def getMoistureValue():
+def getMoistureVoltage():
+    return chan.voltage
+
+
+def getMoistureState():
   raw_value =  chan.value
   state = "N/A"
   if(chan.voltage> 1.8 and chan.voltage <2.3):
      state = "WET"
-  else if (chan.voltage > 2.3 and chan.voltage < 3.0)
+  elif (chan.voltage > 2.3 and chan.voltage < 3.0):
      state = "DRY"
   return state
+ 
+if __name__ == '__main__':
+    while True:
+        print(getMoistureVoltage())
+        time.sleep(1)
 
 
